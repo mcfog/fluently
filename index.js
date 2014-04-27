7aaddd7a4fd0b001eb0c1a09304596fa66670184
McFog W <mcfogw@gmail.com>
2014-04-27 17:19:47 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,122 @@
+"use strict";
+
+var has = Function.prototype.call.bind(Object.prototype.hasOwnProperty);
+
+function isObj(obj) {
+	return obj !== null && typeof obj !== 'undefined';
+}
+function create(obj) {
+	return new Fluently(obj);
+}
+function tap(cb) {
+	/* jshint validthis:true */
+	cb(this);
+
+	return this;
+}
+
+function iteratorMethod(method) {
+	return function(obj) {
+		var flu = this;
+		Object.keys(obj).forEach(function(key) {
+			flu[method](key, obj[key]);
+		});
+
+		return this;
+	};
+}
+
+function Fluently(obj) {
+	switch(true) {
+	case this instanceof Fluently:
+
+		//var builder = new Fluently(target);
+		this._target = obj;
+
+		break;
+
+	case isObj(this):
+		//var builder = Fluently.call(target) or target.xxx = Fluently, target.xxx()
+		return create(this);
+
+	case isObj(obj):
+		//var builder = Fluently(target)
+		return create(obj);
+
+	default:
+		//var builder = Fluently()
+		return create({});
+	}
+}
+
+Fluently.prototype = Object.freeze({
+	"mount": function(name) {
+		name = name || "_fluently_";
+
+		Object.defineProperty(this._target, name, {
+			configurable: false,
+			enumerable: false,
+			value: this,
+			writable: false
+		});
+
+		return this;
+	},
+
+	"define": function(name, value) {
+		if(this._target[name] !== undefined) {
+			throw new Error('Fluently: define something twice');
+		}
+		this._target[name] = value;
+
+		return this;
+	},
+	"defineObj": iteratorMethod('define'),
+
+	"override": function(name, value) {
+		this._target[name] = value;
+
+		return this;
+	},
+	"overrideObj": iteratorMethod('override'),
+
+	"void": function(name) {
+		var method = this._target[name];
+
+		return this.override(name, function() {
+			method.apply(this, arguments);
+
+			return this;
+		});
+	},
+	"setter": function(name, setterName) {
+		if(!setterName) {
+			setterName = "set" + name[0].toUpperCase() + name.slice(1);
+		}
+
+		return this.define(setterName, function(value) {
+			this[name] = value;
+
+			return this;
+		});
+	},
+	"setterObj": iteratorMethod('setter'),
+
+	"tapper": function(name) {
+		return this.define(name, tap);
+	},
+
+	"defineBlock": function(begin, end, block) {
+		create(block).define(end, this._target);
+		this.define(begin, block);
+
+		return this;
+	},
+
+	"tap": tap,
+	"getTarget": function() {
+		return this._target;
+	}
+});
+
+module.exports = Fluently;
---
+++ test/test.js
@@ -0,0 +1,250 @@
+var expect = require('chai').expect;
+var fluently = require('../');
+
+var uniq = (function() {
+	var counter = 0;
+	function random() {
+		return Math.random().toString(36).slice(2);
+	}
+
+	return function() {
+		return 'uniq_' + random() + '_' + counter++;
+	};
+})();
+
+
+describe("constructor & flu.getTarget", function() {
+	it("new fluently(obj) works", function() {
+		var obj = {};
+		var flu = new fluently(obj);
+
+		expect(flu.getTarget()).to.equal(obj);
+	});
+
+	it("fluently(obj) works", function() {
+		var obj = {};
+		var flu = fluently(obj);
+
+		expect(flu.getTarget()).to.equal(obj);
+	});
+
+	it("fluently.call(obj) works", function() {
+		var obj = {};
+		var flu = fluently.call(obj);
+
+		expect(flu.getTarget()).to.equal(obj);
+	});
+
+	it("fluently.call(obj, obj2) obj wins", function() {
+		var obj = {};
+		var flu = fluently.call(obj, {});
+
+		expect(flu.getTarget()).to.equal(obj);
+	});
+
+	it("fluently() works", function() {
+		var flu = fluently();
+
+		expect(JSON.stringify(flu.getTarget())).to.equal('{}');
+	});
+});
+
+
+describe("mount", function() {
+	var flu, obj;
+
+	beforeEach(function() {
+		obj = {};
+		flu = fluently(obj);
+	});
+
+	it("mount() works", function() {
+
+		//chainable api test
+		expect(flu.mount()).to.equal(flu);
+
+		expect(obj._fluently_).to.equal(flu);
+	});
+
+	it("mount('name') works", function() {
+		var name = uniq();
+
+		//chainable api test
+		expect(flu.mount(name)).to.equal(flu);
+
+		expect(obj[name]).to.equal(flu);
+	});
+});
+
+describe("define", function() {
+	var flu, obj, key, value;
+
+	beforeEach(function() {
+		obj = {};
+		flu = fluently(obj);
+		key = uniq();
+		value = {};
+	});
+
+	it("define(key, value) works", function() {
+		//chainable api test
+		expect(flu.define(key, value)).to.equal(flu);
+
+		expect(obj[key]).to.equal(value);
+	});
+
+	it("defineObj({key: value}) works", function() {
+		var defObj = {};
+		defObj[key] = value;
+
+		//chainable api test
+		expect(flu.defineObj(defObj)).to.equal(flu);
+
+		expect(obj[key]).to.equal(value);
+	});
+
+	it("redefine panics", function() {
+		flu.define(key, value);
+		expect(function() {
+			flu.define(key, value);
+		}).to.throw(Error);
+	});
+});
+
+
+describe("override", function() {
+	var flu, obj, key, value;
+
+	beforeEach(function() {
+		obj = {};
+		flu = fluently(obj);
+		key = uniq();
+		value = {};
+		obj[key] = null;
+	});
+
+	it("override(key, value) works", function() {
+		//chainable api test
+		expect(flu.override(key, value)).to.equal(flu);
+
+		expect(obj[key]).to.equal(value);
+	});
+
+	it("overrideObj({key: value}) works", function() {
+		var defObj = {};
+		defObj[key] = value;
+
+		//chainable api test
+		expect(flu.overrideObj(defObj)).to.equal(flu);
+
+		expect(obj[key]).to.equal(value);
+	});
+
+	it("override again works", function() {
+		flu
+			.override(key, null)
+			.override(key, value);
+		expect(obj[key]).to.equal(value);
+	});
+});
+
+
+describe("void", function() {
+	it("works", function() {
+		var passed = false;
+		var obj = {
+			func: function() {
+				passed = true;
+			}
+		};
+		var flu = fluently(obj);
+		expect(flu.void('func')).to.equal(flu);
+		expect(obj.func()).to.equal(obj);
+		expect(passed).to.be.true;
+	});
+});
+
+
+describe("setter", function() {
+	var key, setterKey, obj, value, flu;
+
+	beforeEach(function() {
+		key = uniq();
+		setterKey = uniq();
+
+		obj = {};
+		value = {};
+		flu = fluently(obj);
+	});
+
+	it("works", function() {
+		expect(flu.setter(key, setterKey)).to.equal(flu);
+		expect(obj[setterKey](value)).to.equal(obj);
+		expect(obj[key]).to.equal(value);
+	});
+
+	it("setterObj works", function() {
+		var setterObj = {};
+		setterObj[key] = setterKey;
+
+		expect(flu.setterObj(setterObj)).to.equal(flu);
+		expect(obj[setterKey](value)).to.equal(obj);
+		expect(obj[key]).to.equal(value);
+	});
+});
+
+describe("tapper", function() {
+	it("works", function() {
+		var obj = {};
+		var flu = fluently(obj);
+		var key = uniq();
+
+		var passed = false;
+
+		expect(flu.tapper(key)).to.equal(flu);
+
+		expect(obj[key](function(it) {
+			expect(it).to.equal(obj);
+			passed = true;
+
+			return false;
+		})).to.equal(obj);
+
+		expect(passed).to.be.true;
+	});
+});
+
+describe("tap", function() {
+	it("works", function() {
+		var obj = {};
+		var flu = fluently(obj);
+
+		var passed = false;
+
+		expect(flu.tap(function(it) {
+			expect(it).to.equal(flu);
+			passed = true;
+
+			return false;
+		})).to.equal(flu);
+
+		expect(passed).to.be.true;
+	});
+});
+
+describe("defineBlock", function() {
+	it("works", function() {
+		var obj = {};
+		var flu = fluently(obj);
+		var block = {};
+		var open = uniq();
+		var close = uniq();
+
+		var passed = false;
+
+		expect(flu.defineBlock(open, close, block)).to.equal(flu);
+
+		expect(obj[open]).to.equal(block);
+		expect(block[close]).to.equal(obj);
+	});
+});
